21e720083aca1b971684f470ec358d8db81e336c
ikenna onyenuforo <mcpikebobs@gmail.com>
2022-01-23 10:11:04 -0800

    hello

--- App.js
+++ App.js
@@ -3,10 +3,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { PartA, PartB, PartC } from './components/Parts'
 import AddItem from './components/AddItem'
+import { createStore } from 'redux';
+//import { Provider } from 'react';
+import reducers from './src/reducers'
+import { Provider } from 'react-redux';
 
+const store = createStore(reducers)
 
-export default function App() {
+ export default function App() {
   return (
+    <Provider store = {store}>
     <View style={styles.container}>
       <Text>Funny enough I love working with React native on the linux platform</Text>
       <StatusBar style="auto" />
@@ -15,6 +21,7 @@ export default function App() {
       <PartC />
       <AddItem></AddItem>
     </View>
+    </Provider>
   );
 }
 
